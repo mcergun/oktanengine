e78340fa673601996f19a8117ba3fe6a48d724d4
Mert Can Ergün <mcergun@windowslive.com>
2019-06-07 18:14:40 +0300

    started using newer opengl functions instead of legacy ones

--- oktanengine/main.cpp
+++ oktanengine/main.cpp
@@ -27,17 +27,33 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+	
+	GLuint bufId;
+	// Generate a buffer and give us its ID
+	glGenBuffers(1, &bufId);
+	// Bind newly generated buffer
+	// GL_ARRAY_BUFFER for vertex attributes
+	// Binding is like selecting the buffer for opengl state machine
+	glBindBuffer(GL_ARRAY_BUFFER, bufId);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	// Create and initialize buffer's data store
+	// GL_ARRAY_BUFFER for vertex attributes
+	// size is in bytes
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
